f075a8a5822c86ec5b743e72d578f864ce1636f6
cotezea <145978670+cotezea@users.noreply.github.com>
2023-12-13 18:31:04 +0000

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,1 +1,10 @@
+// Importing Express and the controller functions
+const express = require('express');
+const { getAllItems, addItem } = require('../controllers/itemsController');
+const router = express.Router();
+// Define routes
+router.get('/', getAllItems);
+router.post('/', addItem);
+
+module.exports = router;
 
